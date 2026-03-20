4a4260b46df7c87b5a052afd9d0e1a8d0b01b5ce
Paul12-45 <paulboissard2@gmail.com>
2026-03-20 09:02:35 -0400

    création fichiers rencontre.tsx et rencontre.css

--- examen1-PaulBoissard/src/components/App.tsx
+++ examen1-PaulBoissard/src/components/App.tsx
@@ -1,11 +1,13 @@
 import '../style/App.css'
 import Header from './Header.tsx'
+import Footer from './Footer.tsx'
 
 
 export default function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }
