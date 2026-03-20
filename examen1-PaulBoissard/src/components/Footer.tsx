d5a4640929560f80f0619ef9eb88cf76c0594864
Paul12-45 <paulboissard2@gmail.com>
2026-03-20 09:01:33 -0400

    ajout footer

---
+++ examen1-PaulBoissard/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import '../style/Footer.css'
+
+export default function Footer() {
+
+    return (
+        <>
+            <footer>
+                <p>Copyright Paul Boissard</p>
+                <p>Tout droits réservés Tincat. Une filiale de Tinder.Inc</p>
+            </footer>
+        </>
+    )
+}
